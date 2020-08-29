42cd479aa310b9f5189ee2fa2c6717dce888a4ba
Drew Thompson <drewallynthompson@gmail.com>
2020-08-29 00:47:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-# def square_array(array)
-#   newbie = []
-#   array.each do |array|
-#     newbie << array**2 
-#   end
-#   newbie
-# end
-
 def square_array(array)
-  array.collect { |index| index**2 }
+  newbie = []
+  array.each do |array|
+    newbie << array**2 
+  end
+  newbie
 end
+
+# def square_array(array)
+#   array.collect { |index| index**2 }
+# end
